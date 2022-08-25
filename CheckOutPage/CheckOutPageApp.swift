4231d3bc802b71060ef5454735d2442c2a1e7a35
Temiloluwa <91268094+Ytemiloluwa@users.noreply.github.com>
2022-08-25 11:44:24 +0100

    Initial Commit

---
+++ CheckOutPage/CheckOutPageApp.swift
@@ -0,0 +1,17 @@
+//
+//  CheckOutPageApp.swift
+//  CheckOutPage
+//
+//  Created by Temiloluwa on 25/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct CheckOutPageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CheckOutPage/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  CheckOutPage
+//
+//  Created by Temiloluwa on 25/08/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
